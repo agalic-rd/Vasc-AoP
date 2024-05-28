ddee47e1dcfed14e88e69e6a60f8f7fc9157e4e1
Marc-Aurèle Rivière <36962105+ma-riviere@users.noreply.github.com>
2024-05-28 12:03:48 +0200

    Data ingestion

--- src/scripts/data.R
+++ src/scripts/data.R
@@ -3,3 +3,22 @@
 ####╚═══════     ══════╝####
 
 cli_h2("┗ [SCRIPTS] Loading data ingestion functions")
+
+
+read_vasc_raw <- function(path = configs$data$vasc_raw_data) {
+  
+  col_names <- (
+    openxlsx2::read_xlsx(path, rows = 1:2, col_names = FALSE, fill_merged_cells = TRUE)
+    |> summarize(across(everything(), \(x) paste(na.omit(x), collapse = "__")))
+    |> unlist()
+  )
+  
+  vasc_data <- openxlsx2::read_xlsx(path, start_row = 3, col_names = FALSE)
+  
+  colnames(vasc_data) <- col_names
+  
+  return(
+    vasc_data |> 
+      pivot_longer(contains("__"), names_sep = "__", names_to = c(".value", "Bins"))
+  )
+}
